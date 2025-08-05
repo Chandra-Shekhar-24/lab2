c991ab729b4051a60ab3ff88407d0d390e499ba8
Chandra-Shekhar-24 <chandrashekharbansal.2006@gmail.com>
2025-08-05 10:54:48 +0530

    refactor : program addition of three no.

--- add.java
+++ add.java
@@ -2,7 +2,8 @@ public class add {
     public static void main(String[] args) {
         int a = 5;
         int b=  5;
-        int c = a+b;
-        System.out.println(c);
+        int c = 5;
+        int d = a+b+c;
+        System.out.println(d);
     }
 }
